8cf7364c00053e7955e4bf21386571c98aa33bb6
Bohdan Yurchuk <bohdan.yurchuk@scribblelive.com>
2020-02-11 10:19:59 +0200

    Create basic layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import './App.css';
 
+import ReelsContainer from "../ReelsContainer";
+import Button from "../Button";
+import DebugArea from "../DebugArea";
+import PayTable from "../PayTable";
+import BalanceIndicator from "../BalanceIndicator";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="uk-grid uk-grid-medium">
+          <div className="uk-width-1-1@s">
+              <div className="uk-flex uk-flex-center">
+                <BalanceIndicator balance={1234}/>
+              </div>
+              <div className="uk-flex uk-flex-center">
+                  <ReelsContainer/>
+              </div>
+              <div className="uk-flex uk-flex-center">
+                  <Button value="Spin!" round={true}/>
+              </div>
+          </div>
+          <div>
+              <PayTable/>
+          </div>
+          <div>
+              <DebugArea/>
+          </div>
+
+      </div>
+
     </div>
   );
 }
