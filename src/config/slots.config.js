337e60a87fbe7d1b363ce77140376e3320aa1705
Bohdan Yurchuk <bohdan.yurchuk@scribblelive.com>
2020-02-11 10:19:44 +0200

    Add slot config

---
+++ src/config/slots.config.js
@@ -0,0 +1,38 @@
+const slotConfig = {
+    symbolsList: {
+        'BAR': {
+            img: 'BAR.png',
+            groups: ['BAR'],
+            value: 'BAR',
+        },
+        '2xBAR': {
+            img: '2xBAR.png',
+            groups: ['BAR'],
+            value: '2xBAR',
+        },
+        '3xBAR': {
+            img: '3xBAR.png',
+            groups: ['BAR'],
+            value: '3xBAR',
+        },
+        '7': {
+            img: '7.png',
+            groups: [],
+            value: '7',
+        },
+        'CHERRY': {
+            img: 'CHERRY.png',
+            groups: [],
+            value: 'CHERRY',
+        }
+    },
+    symbolsOrder: [
+        "3xBAR",
+        "BAR",
+        "2xBAR",
+        "7",
+        "CHERRY",
+    ]
+};
+
+export default slotConfig;
